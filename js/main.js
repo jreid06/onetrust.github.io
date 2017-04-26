1b0efe91284b095ae6236b26bec51c3b1237a06b
Jason Reid <jasonbrandon9406@gmail.com>
2017-04-26 11:37:42 +0100

    final changes for today

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+  function loadwow() {
+    new WOW().init();
+  }
+
+  loadwow();
+
+
 
   $('.kai_banner_container').kaiBanner({
     minWidth:1200,
@@ -8,5 +15,25 @@ $(document).ready(function(){
 
   $('.kai_banner2_container').kaiBanner();
 
-  
+  function scrollDown(selector){
+      document.querySelector(selector).scrollIntoView({ behavior: 'smooth' });
+  }
+
+  $('#sec1').click(function(){
+      scrollDown('#sc1');
+  })
+
+  $('#sec2').click(function(){
+      scrollDown('#sc2');
+  })
+
+  $('#sec3').click(function(){
+      scrollDown('#sc3');
+  })
+
+  $('#sec4').click(function(){
+      scrollDown('#sc4');
+  })
+
+
 })
