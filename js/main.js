e950951513b9945d92195b2175b6f6db764c6ec8
Jason Reid <jasonbrandon9406@gmail.com>
2017-04-26 08:15:08 +0100

    all files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.kai_banner_container').kaiBanner({
+    minWidth:1200,
+    intervalTime:10000,
+    highlightClass:'highlight'
+  });
+
+  $('.kai_banner2_container').kaiBanner();
+
+  
+})
